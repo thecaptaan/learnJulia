58981e7373bce895da92c8ddf468e017a3600030
The Captaan <thecaptaan@proton.me>
2024-02-16 00:45:00 +0530

    print in julia

---
+++ 01 print.jl
@@ -0,0 +1,13 @@
+# print string without a newline
+print("Hello, World!")
+
+# print string with a newline
+println("Hello, World!")
+
+# print string with different colors
+printstyled("Hello, World! ", color=:red)
+
+# print string with different colors and styles
+printstyled("Hello, World! ", color=:blue, bold=true)
+
+# checkout of the print function official documentation for more details
