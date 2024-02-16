538a717a2a369ed37864fb8fcfa189f5f81faf56
The Captaan <thecaptaan@proton.me>
2024-02-16 22:41:54 +0530

    ternary operator

---
+++ 04 ternary.jl
@@ -0,0 +1,6 @@
+# usage of ternary operator
+# check if person is male or female
+print("Enter 'Male' or 'Female': ")
+gender = readline()
+
+gender == "Male" ? println("Person is Male") : println("Person is Female")
